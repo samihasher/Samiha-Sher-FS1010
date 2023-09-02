2049f0aef1a22a01de3de648e14c6b72e81e34cd
samihasher <you@example.com>
2023-09-01 22:15:08 -0400

    Update AboutUs page

---
+++ src/Pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+const AboutUs = () => {
+    return (
+      <div>
+        <h2>About Us</h2>
+        <p>
+          Welcome to MySkin! MySkin is an easy-to-use skincare routine tracker. Easily build your own routines and track the products you use. It has never been easier to build a custom skincare routine that makes you feel in control.
+        </p>
+      </div>
+    );
+  };
+  
+  export default AboutUs;
